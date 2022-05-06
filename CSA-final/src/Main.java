1abc268b3f187a9a1bff8be6264dc3dc4a379bad
Someone117 <sabadareen@gmal.com>
2022-05-06 14:55:19 -0700

    Resolved errors

--- CSA-final/src/Main.java
+++ CSA-final/src/Main.java
@@ -1,8 +1,13 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Word[] w = Word.fromFile("possible words.csv");
+        try {
+            Word.fromFile("CSA-final/res/possible words.csv");
+        } catch (IOException e) { e.printStackTrace(); }
+        Word[] w = Word.getAllWords();
         System.out.println(Arrays.toString(w));
     }
 }
--- CSA-final/src/Word.java
+++ CSA-final/src/Word.java
@@ -1,12 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Word {
+    private static Word[] allWords;
+
     private final String word;
+    private final char[] chars;
     public Word(String w) {
         word = w;
+        chars = w.toCharArray();
     }
 
     public String getWord() {
@@ -30,45 +34,40 @@ public class Word {
         return !word.contains(s);
     }
 
-    public boolean wordleGetOutOfOrder(String s) {
-        String[] parts = s.split("-");
-        for (String part : parts) {
-            if (notPartOf(part)) {
-                return false;
-            }
-        }
-        return true;
-    }
     public boolean wordleGetInOrder(String s) {
-        String[] parts = s.split("-");
-        for (int i = 0; i < parts.length; i++) {
-            if (!word.subSequence(i,i+parts[i].length()).equals(parts[i]) && !parts[i].equals("")) {
-                return false;
+        char[] parts = s.toCharArray();
+        for(int i = 0; i < chars.length; i++) {
+            if(parts[i] != '-') {
+                if(parts[i] != chars[i]) return false;
             }
         }
         return true;
     }
 
-    public static Word[] fromFile(String fileName) { //TODO: no work
-        Word[] words = new Word[0];
-        String line = "";
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            int length = (int) br.lines().count();
-            words = new Word[length];
-            int i = 0;
-            System.out.println(br.readLine());
-            while ((line = br.readLine()) != null) {
-                words[i] = new Word(line);
-                i++;
-            }
+    public static void fromFile(String fileName) throws IOException {
+        Scanner scanner = new Scanner(new File(fileName));
+        ArrayList<Word> wordsList = new ArrayList<>();
+        while(scanner.hasNextLine())
+        {
+            wordsList.add(new Word(scanner.nextLine()));
         }
-        catch (IOException e) { e.printStackTrace(); }
-        return words;
+        allWords = wordsList.toArray(new Word[0]);
     }
 
     @Override
     public String toString() {
         return word;
+    }
+
+    public static Word[] getAllWords() {
+        return allWords;
+    }
+
+    public static void setAllWords(Word[] allWords) {
+        Word.allWords = allWords;
+    }
+
+    public char[] getChars() {
+        return chars;
     }
 }
