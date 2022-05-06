6ba6bf586eec724410eff7cfc975ffb027821ef5
GoldenGreninjaZ <105028016+GoldenGreninjaZ@users.noreply.github.com>
2022-05-06 14:44:48 -0700

    Update Main.java
    Added a Scanner object and added comments

--- CSA-final/src/Main.java
+++ CSA-final/src/Main.java
@@ -2,8 +2,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    
+    //Creating a scanner object to take in words from users
+    public Scanner scan = new Scanner(System.in);
+    
+    //creaing an ArrayList and printing the words out on display from the possible words file
     public static void main(String[] args) {
         Word[] w = Word.fromFile("possible words.csv");
         System.out.println(Arrays.toString(w));
     }
+    
+    
+    
 }
