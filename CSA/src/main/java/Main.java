0235573738e6c681b49c9a92b55499fdbe0e0f6d
Someone117 <sabadareen@gmal.com>
2022-05-06 14:19:58 -0700

    Rework, no longer using Maven

--- CSA/src/main/java/Main.java
+++ CSA/src/main/java/Main.java
@@ -1,11 +1,12 @@
-package main.java;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Word[] w = Word.fromFile("CSA/src/main/resources/possible words.csv");
+        main.java.Word[] w = main.java.Word.fromFile("CSA/src/main/resources/possible words.csv");
         System.out.println(Arrays.toString(w));
+
+
     }
 
 }
